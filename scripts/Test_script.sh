3216ac7123818ef69503f946d2d5a7316122465a
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-04-14 10:32:58 +0200

    test script

---
+++ scripts/Test_script.sh
@@ -0,0 +1,36 @@
+
+# Set number of processes (1 per GPU)
+num_process=2
+master_port=29500  # or any free port
+
+# Configure Accelerate for Kaggle (might require setup beforehand)
+accelerate accelerate launch \
+  --multi_gpu \
+  --num_processes $num_process \
+  --main_process_port $master_port \
+  --mixed_precision bf16 \
+  run_main.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1_small_small.csv \
+  --model_id ETTh1_small_small \
+  --model TimeLLM \
+  --data Traffic \
+  --features M \
+  --seq_len 512 \
+  --label_len 48 \
+  --pred_len 96 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des Exp \
+  --itr 1 \
+  --d_model 32 \
+  --d_ff 128 \
+  --batch_size 2 \
+  --learning_rate 0.01 \
+  --llm_layers 32 \
+  --train_epochs 1 \
+  --model_comment TimeLLM-ETTh1
