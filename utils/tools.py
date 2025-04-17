8ccad0515760e4961f78c2cc0da1ed61b51f16f6
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-04-17 18:42:22 +0200

    test

--- utils/tools.py
+++ utils/tools.py
@@ -306,12 +306,12 @@ def vali(args, accelerator, model, vali_data, vali_loader, criterion, mae_metric
 
         #or
         fig,ax = plt.subplots(figsize=(10,5))
-        ax.plot(all_batch_dates[0], actuals[0], label = 'Actual') #hanno una struttura del tipo 40,1,90
+        #ax.plot(all_batch_dates[0], actuals[0], label = 'Actual') #hanno una struttura del tipo 40,1,90
         #ax.plot(dates, actuals[0], label = 'Actual') #hanno una struttura del tipo 40,1,90
-        #ax.plot(actuals, label = 'Actual')
-        ax.plot(all_batch_dates[0], predictions[0], label = 'Predictions', color='red')
+        ax.plot(actuals[0], label = 'Actual')
+        #ax.plot(all_batch_dates[0], predictions[0], label = 'Predictions', color='red')
         #ax.plot(dates, predictions[0], label = 'Predictions', color='red')
-        #ax.plot(predictions, label = 'Predictions', color='red')
+        ax.plot(predictions[0], label = 'Predictions', color='red')
         ax.legend()
         ax.set_xlabel('Timestamp')
         ax.set_ylabel('Affluence')
@@ -320,12 +320,12 @@ def vali(args, accelerator, model, vali_data, vali_loader, criterion, mae_metric
         test_writer.add_figure(f"Prediction vs Actual Test Epoch{epoch + 1} (simple plot)", fig)
 
         fig,ax = plt.subplots(figsize=(10,5))
-        ax.plot(all_batch_dates[0], actuals_norm[0], label = 'Actual')
+        #ax.plot(all_batch_dates[0], actuals_norm[0], label = 'Actual')
         #ax.plot(dates, actuals_norm[0], label = 'Actual')
-        #ax.plot(actuals, label = 'Actual Normal')
+        ax.plot(actuals[0], label = 'Actual Normal')
         ax.plot(all_batch_dates[0], predictions_norm[0], label = 'Predictions', color='red')
         #ax.plot(dates, predictions_norm[0], label = 'Predictions', color='red')
-        #ax.plot(predictions, label = 'Predictions Normal', color='red')
+        ax.plot(predictions[0], label = 'Predictions Normal', color='red')
         ax.legend()
         ax.set_xlabel('Timestamp')
         ax.set_ylabel('Affluence')
