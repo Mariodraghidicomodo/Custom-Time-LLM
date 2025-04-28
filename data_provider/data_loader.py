47be73ca1c558057c2cf8349edba63ee09c9a326
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-04-28 13:19:57 +0200

    test with __str__

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -297,8 +297,8 @@ class Dataset_Custom(Dataset):  #PROVARE A USARE QUESTO PER CREARE IL DATASET AL
 #----- AGGIUNTE
         test_dates = df_raw[['date']][border1:border2].values #add
         
-        #self.date_string = test_dates #add (questo funziona)
-        self.date_string = test_dates.astype(str)
+        self.date_string = test_dates #add (questo funziona)
+        #self.date_string = test_dates.astype(str)
         
         if self.set_type == 2: #se è test
             print('self.date_string: ',type(self.date_string))
@@ -333,9 +333,8 @@ class Dataset_Custom(Dataset):  #PROVARE A USARE QUESTO PER CREARE IL DATASET AL
 #----- AGGIUNTE
         if self.set_type != 0: #se e il test o vali creo anche un batch per le date
             #seq_y_dates = self.date_string[r_begin:r_end] #ricordo che è un numpy array non più un df (originale)
-            #seq_y_dates = self.date_string[r_begin:r_end].__str__ #(da provare)
+            seq_y_dates = self.date_string[r_begin:r_end].__str__ #(da provare)
             #seq_y_dates = self.date_string[r_begin:r_end].astype(str).tolist() #(questo funziona)
-            seq_y_dates = self.date_string[r_begin:r_end].to_numpy()
             '''
             print('index: ',index)
             print('s_begin:', s_begin)
