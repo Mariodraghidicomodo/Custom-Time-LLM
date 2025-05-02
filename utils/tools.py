3131097598f5800b470b8470086da7fca557a484
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-05-02 17:02:21 +0200

    comment the plots

--- utils/tools.py
+++ utils/tools.py
@@ -332,7 +332,7 @@ def vali(args, accelerator, model, vali_data, vali_loader, criterion, mae_metric
         #plot_test(predictions, predictions_norm, actuals, actuals_norm, dates, epoch, args)
     #    plot_test(predictions_flat, predictions_flat_norm, actuals_flat, actuals_flat_norm, dates['date'], epoch, args)
 
-    if format == 'vali':
+    '''if format == 'vali':
 
         #title = f'Predictions vs Actuals Vali Epoch {epoch + 1}'
         #title_normal = f'Predictions Normal vs Actuals Normal Vali Epoch {epoch + 1}'
@@ -367,9 +367,9 @@ def vali(args, accelerator, model, vali_data, vali_loader, criterion, mae_metric
         ax.set_xlabel('Timestamp')
         ax.set_ylabel('Affluence')
         ax.set_title(f'Prediction vs Actual NORMAL Vali Epoch{epoch + 1}')
-        test_writer.add_figure(f"Prediction Normal vs Actual Normal Vali Epoch{epoch + 1} (simple plot)", fig)
+        test_writer.add_figure(f"Prediction Normal vs Actual Normal Vali Epoch{epoch + 1} (simple plot)", fig)'''
 
-    if format == 'test':
+    '''if format == 'test':
 
         #title = f'Predictions vs Actuals Test Epoch {epoch + 1}'
         #title_normal = f'Predictions Normal vs Actuals Normal Test Epoch {epoch + 1}'
@@ -442,7 +442,7 @@ def vali(args, accelerator, model, vali_data, vali_loader, criterion, mae_metric
         ax.set_xlabel('Timestamp')
         ax.set_ylabel('Affluence')
         ax.set_title(f'Prediction vs Actual NORMAL Test Epoch{epoch + 1} blocco 1')
-        test_writer.add_figure(f"PN vs AN Test Epoch{epoch + 1} (simple plot) blocco 1", fig)
+        test_writer.add_figure(f"PN vs AN Test Epoch{epoch + 1} (simple plot) blocco 1", fig)'''
 
 
     test_writer.close() #close writer
