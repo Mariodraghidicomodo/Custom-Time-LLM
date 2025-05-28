163f6ea6dc9d7ee062233f94d9f156d652f324ef
Mariodraghidicomodo <alessandropedron98@gmail.com>
2025-05-28 11:55:45 +0200

    new test

--- scripts/Test_Script_mini_15min_poi_Arena_gpt2.sh
+++ scripts/Test_Script_mini_15min_poi_Arena_gpt2.sh
@@ -10,7 +10,7 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --is_training 1 \
   --root_path ./dataset/final_15min_df_poi/ \
   --data_path frequency_15min_df_49_year2019.csv \
-  --model_id Arena_Verona_2019_gpt2 \
+  --model_id Arena_Verona_2019 \
   --model TimeLLM \
   --llm_model GPT2 \
   --llm_dim 768 \
@@ -19,8 +19,8 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --target frequency \
   --freq 15min \
   --scale True \
-  --seq_len 96 \
-  --label_len 24 \
+  --seq_len 32 \
+  --label_len 12 \
   --pred_len 1 \
   --enc_in 1 \
   --dec_in 1 \
@@ -29,13 +29,13 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --moving_avg 96 \
   --des Exp \
   --itr 1 \
-  --d_model 32 \
-  --d_ff 128 \
-  --n_head 4 \
-  --batch_size 12 \
-  --eval_batch_size 8 \
+  --d_model 16 \
+  --d_ff 64 \
+  --n_head 2 \
+  --batch_size 6 \
+  --eval_batch_size 6 \
   --learning_rate 0.01 \
-  --llm_layers 8 \
-  --train_epochs 10 \
+  --llm_layers 2 \
+  --train_epochs 1 \
   --seasonal_patterns Daily \
   --model_comment TimeLLM_Arena_Verona_2019_15min_1epochs
